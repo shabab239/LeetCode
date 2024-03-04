03c6d9324e22e1d728a75da09332351151b561f0
Shabab Ahmed <sabab239.1@gmail.com>
2024-03-05 01:14:19 +0600

    3. Merge Strings Alternately

---
+++ src/MergeStringsAlternately.java
@@ -0,0 +1,23 @@
+public class MergeStringsAlternately {
+
+    public static String mergeAlternately(String word1, String word2) {
+        int minLength = Math.min(word1.length(), word2.length());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < minLength; i++) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(i));
+        }
+
+        sb.append(word1.substring(minLength));
+        sb.append(word2.substring(minLength));
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("ab", "pqrs"));
+    }
+
+}
